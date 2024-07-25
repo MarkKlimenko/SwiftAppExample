01fac4d651b6cb1a21ced552e185285076a2847e
markklimenko <mark.klimenko.w@gmail.com>
2024-07-24 19:07:34 -0600

    Add list

--- SwiftApp/RestaurantListView.swift
+++ SwiftApp/RestaurantListView.swift
@@ -9,35 +9,60 @@ import Foundation
 import SwiftUI
 
 struct RestaurantListView: View {
-    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl"
-    , "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"
     ]
+    
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"
+    ]
+    
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "TeaHouse", "Austrian/Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American/Seafood", "American", "American", "Breakfast & Brunch", "Coffee &Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
     
     var body: some View {
         List {
-            ForEach(restaurantNames, id: \.self) { restaurantName in
-                HStack(alignment: .top, spacing: 20) {
-                    Image(restaurantName).resizable()
-                        .frame(width: 120, height: 118)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                    
-                    VStack(alignment: .leading){
-                        Text(restaurantName)
-                        .font(.system(.title2, design: .rounded))
-                        
-                        Text("Type")
-                        .font(.system(.body, design: .rounded))
-                        
-                        Text("Location")
-                        .font(.system(.subheadline, design: .rounded))
-                        .foregroundStyle(.gray)
-                    }
-                }
+            ForEach(0...restaurantNames.count - 1, id: \.self) { index in
+                FullImageRow(
+                    imageName: restaurantNames[index],
+                    name: restaurantNames[index],
+                    type: restaurantTypes[index],
+                    location: restaurantLocations[index]
+                )
             }.listRowSeparator(.hidden)
         }.listStyle(.plain)
     }
 }
 
-#Preview {
+#Preview("Dark mode") {
     RestaurantListView()
+        .preferredColorScheme(.dark)
+}
+
+struct FullImageRow: View {
+    var imageName: String
+    var name: String
+    var type: String
+    var location: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Image(imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+        
+            VStack(alignment: .leading){
+                Text(name)
+                    .font(.system(.title2, design: .rounded))
+                
+                Text(type)
+                    .font(.system(.body, design: .rounded))
+                
+                Text(location)
+                    .font(.system(.subheadline, design: .rounded))
+                    .foregroundStyle(.gray)
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+        }
+    }
 }
